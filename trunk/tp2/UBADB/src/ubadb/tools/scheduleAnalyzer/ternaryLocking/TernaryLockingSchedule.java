998661f2b277b0d77bc01c45b78b6929d772a76d
amelendez <amelendez@ac7c2928-1c98-11de-82b5-194b17410922>
2009-05-27 01:48:54 +0000

    construccion del grafo de locking ternario

--- trunk/tp2/UBADB/src/ubadb/tools/scheduleAnalyzer/ternaryLocking/TernaryLockingSchedule.java
+++ trunk/tp2/UBADB/src/ubadb/tools/scheduleAnalyzer/ternaryLocking/TernaryLockingSchedule.java
@@ -1,7 +1,10 @@
 package ubadb.tools.scheduleAnalyzer.ternaryLocking;
 
+import java.util.Iterator;
+
 import ubadb.tools.scheduleAnalyzer.common.Action;
 import ubadb.tools.scheduleAnalyzer.common.Schedule;
+import ubadb.tools.scheduleAnalyzer.common.ScheduleArc;
 import ubadb.tools.scheduleAnalyzer.common.ScheduleGraph;
 import ubadb.tools.scheduleAnalyzer.common.ScheduleType;
 import ubadb.tools.scheduleAnalyzer.common.results.LegalResult;
@@ -45,28 +48,83 @@ public class TernaryLockingSchedule extends Schedule
 	{
 		//TODO: Completar Construir Grafo ternario - Andres
 		//Se deben agregar arcos entre T1 -> T2 cuando:
-		//- T1 hace Rlock de un ítem A y T2 luego hace WLock de A
-		//- T1 hace Wlock de un ítem A y T2 luego hace RLock de A
-		//- T1 hace Wlock de un ítem A y T2 luego hace WLock de A
-		//OBS1: No hay arcos entre RLocks sobre el mismo ítem 
+		//- T1 hace Rlock de un tem A y T2 luego hace WLock de A
+		//- T1 hace Wlock de un tem A y T2 luego hace RLock de A
+		//- T1 hace Wlock de un tem A y T2 luego hace WLock de A
+		//OBS1: No hay arcos entre RLocks sobre el mismo tem 
 		//OBS2: No agregar arcos que se deducen por transitividad
 		
-		return null;
+		ScheduleGraph graph = new ScheduleGraph();
+		
+		// Se agregan las transactions al grafo
+		for (Iterator iter = getTransactions().iterator(); iter.hasNext();){
+			graph.addTransaction((String)iter.next());
+		}
+		
+		//Se agregan las dependencias al grafo
+		for (int i = 0; i < getActions().size(); i++) {
+			addArcs(graph,i);
+		}
+				
+		return graph;
 	}
 	//[end]
 
+	private void addArcs(ScheduleGraph graph, int indexArc)
+	{
+		TernaryLockingAction action1 = (TernaryLockingAction) getActions().get(indexArc);
+		String item1 = action1.getItem();
+		String T1 = action1.getTransaction();
+		TernaryLockingAction action2;
+		String item2;
+		String T2;
+		if(action1.reads()){
+			for (int i = indexArc + 1; i < getActions().size(); i++){
+				action2 = (TernaryLockingAction) getActions().get(i);
+				item2 = action2.getItem();
+				T2 = action2.getTransaction();
+
+				//si ocurre un read del mismo elemento, sigo
+				if(action2.reads() && item1.equals(item2) && !T1.equals(T2)){
+					break;
+				}
+				// Si encuentro un write sobre el mismo item y de transacciones diferentes agrego un arco
+				if(action2.writes() && item1.equals(item2) && !T1.equals(T2)){
+					ScheduleArc arc = new ScheduleArc(T1,T2,indexArc,i);
+					graph.addArc(arc);
+					break;
+				}
+				
+			}
+		}else if(action1.writes()){
+			for (int i = indexArc + 1; i < getActions().size(); i++){
+				action2 = (TernaryLockingAction) getActions().get(i);
+				T2 = action2.getTransaction();
+				item2 = action2.getItem();
+				
+				// Si encuentro un action sobre el mismo item y de transacciones diferentes agrego un arco.
+				if((action2.reads() || action2.writes()) && item1.equals(item2) && !T1.equals(T2)){
+					ScheduleArc arc = new ScheduleArc(T1,T2,indexArc,i);
+					graph.addArc(arc);
+//					if(action2.writes()) //Si el action es un write termino de buscar
+					break;
+				}
+			}
+		}
+	}
+	
 	//[start] analyzeLegality
 	@Override
 	public LegalResult analyzeLegality()
 	{
 		//TODO: Completar Legabilidad Ternaria - Fabrizio
 		//Un schedule es legal cuando:
-		//- Cada transacción T posee como máximo un commit
+		//- Cada transaccin T posee como mximo un commit
 		//- Si T hace RLOCK A o WLOCK A, luego debe hacer UNLOCK A
 		//- Si T hace UNLOCK A, antes debe haber hecho RLOCK A o WLOCK A
 		//- Si T hace RLOCK A o WLOCK A, no puede volver a hacer RLOCK A o WLOCK A a menos que antes haya hecho UNLOCK A
-		//- Si T hace RLOCK A, ninguna otra transacción T' puede hacer WLOCK A hasta que T libere a A
-		//- Si T hace WLOCK A, ninguna otra transacción T' puede hacer RLOCK A o WLOCK A hasta que T libere a A
+		//- Si T hace RLOCK A, ninguna otra transaccin T' puede hacer WLOCK A hasta que T libere a A
+		//- Si T hace WLOCK A, ninguna otra transaccin T' puede hacer RLOCK A o WLOCK A hasta que T libere a A
 		return null;
 	}
 	//[end]
