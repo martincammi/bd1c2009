b10b7b9d7d854c08bc87939c009f7ec31ac90738
amelendez <amelendez@ac7c2928-1c98-11de-82b5-194b17410922>
2009-05-27 01:41:13 +0000

    cambios pedidos por la catedra

--- trunk/tp2/UBADB/src/ubadb/tools/scheduleAnalyzer/common/results/SerializabilityResult.java
+++ trunk/tp2/UBADB/src/ubadb/tools/scheduleAnalyzer/common/results/SerializabilityResult.java
@@ -2,17 +2,15 @@ package ubadb.tools.scheduleAnalyzer.common.results;
 
 import java.util.List;
 
-import ubadb.tools.scheduleAnalyzer.common.Action;
-
 public class SerializabilityResult
 {
 	private boolean isSerializable;
-	private List<List<Action>> possibleExecutions; //Si es serializable: Lista de posibles ejecuciones
-	private List<String> cycle;	//Si no lo es: Lista de transacciones que forman un ciclo (si hay más, que devuelva alguno). El último arco que cierra el ciclo es implícito
+	private List<List<String>> possibleExecutions; //Si es serializable: Lista de posibles ejecuciones
+	private List<String> cycle;	//Si no lo es: Lista de transacciones que forman un ciclo (si hay ms, que devuelva alguno). El ltimo arco que cierra el ciclo es implcito
 	private String message; // Cualquier mensaje adicional
 	
 	public SerializabilityResult(boolean isSerializable,
-			List<List<Action>> possibleExecutions, List<String> cycle, String message)
+			List<List<String>> possibleExecutions, List<String> cycle, String message)
 	{
 		this.isSerializable = isSerializable;
 		this.possibleExecutions = possibleExecutions;
@@ -25,7 +23,7 @@ public class SerializabilityResult
 		return isSerializable;
 	}
 
-	public List<List<Action>> getPossibleExecutions()
+	public List<List<String>> getPossibleExecutions()
 	{
 		return possibleExecutions;
 	}
