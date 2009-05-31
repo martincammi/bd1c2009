f6afdb6fa2a4806e2d8fdba424f49afcd34869eb
amelendez <amelendez@ac7c2928-1c98-11de-82b5-194b17410922>
2009-05-31 23:20:20 +0000

    para dibujar grafo de precedencia

---
+++ tp2/UBADB/src/ubadb/tools/scheduleAnalyzer/common/GrafoApplet.java
@@ -0,0 +1,118 @@
+package ubadb.tools.scheduleAnalyzer.common;
+
+import java.applet.Applet;
+import java.awt.BorderLayout;
+import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.Shape;
+import java.awt.geom.AffineTransform;
+import java.awt.geom.GeneralPath;
+
+public class GrafoApplet extends Applet {
+
+	public GrafoApplet(ScheduleGraph grafo) {
+		DibujaGrafo canvas = new DibujaGrafo(grafo);
+		this.add(canvas, BorderLayout.CENTER);
+	}
+
+	public Dimension preferredSize() {
+		return new Dimension(800, 600);
+	}
+}
+
+class DibujaGrafo extends Canvas {
+	ScheduleGraph grafo;
+
+	public DibujaGrafo(ScheduleGraph grafo) {
+		this.grafo = grafo;
+		setBackground(Color.white);
+	}
+
+	public void update(Graphics g) {
+		paint(g);
+	}
+
+	public void paint(Graphics g) {
+		Dimension d = preferredSize();
+		int diametroNodo = 10;
+		int radioNodo = diametroNodo / 2;
+		Image imagenRect = createImage(d.width, d.height);
+		int total = this.grafo.getTransactions().size();
+		int aumento = 100;
+
+		for (int i = 1; i < total + 1; i++) {
+			double coordX = d.width / 2
+					+ Math.sin(Math.toRadians((360 / total) * i)) * aumento;
+			double coordY = d.height / 2
+					+ Math.cos(Math.toRadians((360 / total) * i)) * aumento;
+
+			g.setColor(Color.blue);
+			g.fillOval((int) coordX - radioNodo, (int) coordY - radioNodo, diametroNodo, diametroNodo);
+			g.drawString("T" + i, (int) coordX + 10, (int) coordY);
+		}
+
+		System.out.println("arcos="+this.grafo.getArcs().size());
+		for (int j = 0; j < this.grafo.getArcs().size(); j++) {
+				
+			String st1 = this.grafo.getArcs().get(j).getStartTransaction().substring(1);
+			String st2 = this.grafo.getArcs().get(j).getEndTransaction().substring(1);
+			int it1 = new Integer(st1).intValue();
+			int it2 = new Integer(st2).intValue();
+				
+			int coordX1 = (int) (d.width / 2 + Math.sin(Math.toRadians((360 / total) * it1))* aumento);
+			int coordY1 = (int) (d.height / 2 + Math.cos(Math.toRadians((360 / total) * it1))* aumento);
+			int coordX2 = (int) (d.width / 2 + Math.sin(Math.toRadians((360 / total) * it2))* aumento);
+			int coordY2 = (int) (d.height / 2 + Math.cos(Math.toRadians((360 / total) * it2))* aumento);
+			
+			
+			double rotacion; 
+			if(it2>it1){
+				rotacion = (it1*(360/total)) + ((it2-it1)*(360/(total*2)));
+			}else{
+				rotacion = (it1*(360/total)) + ((total-it1+it2)*(360/(total*2)));
+			}
+			if(rotacion>360) rotacion-=360;
+			
+			dibujarFlecha(g, coordX1, coordY1, coordX2, coordY2,rotacion);
+		}
+
+	}
+
+	private void dibujarFlecha(Graphics g, int x0, int y0, int x1, int y1,double rotacion) {
+		Graphics2D graphics2D = (Graphics2D) g;
+		int b = 12;
+		AffineTransform at = AffineTransform.getTranslateInstance(x1, y1);
+		double theta = 25;
+
+		GeneralPath path = new GeneralPath();
+
+		int x;
+		int y;
+			
+		x = (int) ( -b * Math.cos(Math.toRadians(theta)));
+		y = (int) ( -b * Math.sin(Math.toRadians(theta)));
+		path.moveTo(0, 0);
+		path.lineTo(x, y);
+		x = (int) ( -b * Math.cos(Math.toRadians(theta)));
+		y = (int) ( b * Math.sin(Math.toRadians(theta)));
+		path.moveTo(0, 0);
+		path.lineTo(x, y);
+
+		at.rotate(Math.toRadians(360-rotacion));
+		Shape shape = at.createTransformedShape(path);
+
+		graphics2D.setPaint(Color.black);
+		graphics2D.draw(shape);
+
+		g.setColor(Color.black);
+		g.drawLine(x0, y0, x1, y1);
+	}
+
+	public Dimension preferredSize() {
+		return new Dimension(800, 600);
+	}
+}
